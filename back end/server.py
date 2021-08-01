df4b3052794cb8c02e48e053bf6a853213e81790
jakebasel <83322584+jakebasel@users.noreply.github.com>
2021-07-31 21:35:55 -0400

    Add files via upload

---
+++ back end/server.py
@@ -0,0 +1,134 @@
+from flask import Flask, abort, request, render_template
+from data import data
+import json
+from flask_cors import CORS
+from config import db, parse_json
+
+app = Flask(__name__)
+CORS(app)
+
+# dictionary
+me = {
+    "name" : "Jake",
+    "last" : "Basel",
+    "email" : "jake.basel@gmail.com",
+}
+
+# list
+products = data
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return "Hello from Flask"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/about/name")
+def name():
+    return me["name"]
+
+@app.route("/about/fullname")
+def full_name():
+    return me["name"] + " " +me["last"]
+
+
+@app.route("/api/catalog")
+def get_catalog():
+    cursor = db.products.find({})
+    catalog = [item for item in cursor]
+
+    return parse_json(catalog)
+
+
+@app.route("/api/catalog", methods=['POST'])
+def save_product():
+    prod = request.get_json()
+    db.products.insert(prod)
+    return parse_json(prod)
+
+@app.route("/api/order", methods=['POST'])
+def save_order():
+    order = request.get_json()
+    total = 0.00
+    for prod in order["products"]:
+        qnty = prod["quantity"]
+        price = prod["price"]
+        total += (qnty*price)
+            
+    code = order["couponCode"]
+    if(len(code)>0):
+        cursor = db.couponCodes.find({"code":code})
+        for coupon in cursor:
+            discount = total * (coupon["discount"]/100)
+            total = total - discount
+
+    order["total"] = total
+
+    #calculate the total
+    #verify if there is coupon code on order
+    # if so, verify disoucnt
+    # apply discount
+    db.orders.insert(order)
+    return parse_json(order)
+
+
+@app.route("/api/catalog/<category>")
+def get_product_by_category(category):
+    data = db.products.find({"category": category})
+    results = [item for item in data]
+    return parse_json(results)
+
+@app.route("/api/discountCode/<code>")
+def validate_discount(code):
+    data = db.couponCodes.find({"code":code})
+    for code in data:
+        return parse_json(code)
+    return parse_json({"error":True,"reason":"Invalid Code"})
+    
+@app.route("/api/catalog/id/<id>")
+def get_product_by_id(id):
+    for prod in products:
+        if(prod["_id"] == id):
+            return json.dumps(prod)
+    abort(404)
+
+@app.route("/api/catalog/cheapest")
+def get_cheapest_product():
+    cheapest_product = products[0]
+    for prod in products:
+        if prod["price"] < cheapest_product["price"]:
+            cheapest_product = prod
+    return json.dumps(cheapest_product)
+
+
+@app.route("/api/categories")
+def get_categories():
+    data = db.products.find({})
+    unique_categories = []
+    for prod in data:
+        cat = prod["category"]
+        if cat not in unique_categories:
+            unique_categories.append(cat)
+    return parse_json(unique_categories)
+
+@app.route("/api/test")
+def test_data_manipulation():
+    test_data = db.test.find({})    
+    print(test_data)
+
+    return parse_json(test_data[0])
+
+@app.route("/test/populatecodes")
+def test_populate_codes():
+    db.couponCodes.insert({"code":"qwerty","discount":10})
+    db.couponCodes.insert({"code":"ploop","discount":7})
+    db.couponCodes.insert({"code":"cheaper","discount":5})
+    db.couponCodes.insert({"code":"abab1212","discount":95})
+
+    return "Codes registered"
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
